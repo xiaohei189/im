c1b8cd488dbca47ec4e47ae5e40d2cc7b1bbb71e
xiaohei <jueduizhengdian@aliyun.com>
2022-04-17 22:13:06 +0800

    intit

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,18 +1,15 @@
 //! Simple websocket client.
+use std::sync::Arc;
+use std::time::Duration;
 use std::{io, thread};
-use std::time::{Duration, Instant};
-use websocket::{ClientBuilder, OwnedMessage};
-use websocket::r#async::client::{Client, ClientNew};
-use websocket::r#async::TcpStream;
-use websocket::futures::{Future, Stream, Sink};
-use websocket::Message;
-use tokio::runtime::Builder;
-use tokio::sync::mpsc;
-use tokio::time;
-use tokio::time::Interval;
-use tokio_stream::wrappers::UnboundedReceiverStream;
-#[tokio::main]
 
+use actix_web::web::Bytes;
+use awc::ws;
+use futures_util::{SinkExt as _, StreamExt as _};
+use tokio::{select, sync::mpsc};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
+#[actix_web::main]
 async fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
@@ -20,8 +17,10 @@ async fn main() {
 
     let (cmd_tx, cmd_rx) = mpsc::unbounded_channel();
     let mut cmd_rx = UnboundedReceiverStream::new(cmd_rx);
+    let tx = Arc::new(cmd_tx);
     //
     // // run blocking terminal input reader on separate thread
+    let cmd_tx = Arc::clone(&tx);
     let input_thread = thread::spawn(move || loop {
         let mut cmd = String::with_capacity(32);
 
@@ -33,64 +32,52 @@ async fn main() {
         cmd_tx.send(cmd).unwrap();
     });
 
-    for i in 0..15000 {
-        log::info!("create client {}",i);
+    //  heartbeat
+    let ping_tx = Arc::clone(&tx);
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(5));
+        ping_tx.send(String::from("ping")).unwrap();
+    });
 
-        let mut client = ClientBuilder::new("ws://127.0.0.1:8080/ws").unwrap().connect_insecure().unwrap();
+    // create 10 thread
+    let (res, mut ws) = awc::Client::new()
+        .ws("ws://127.0.0.1:8080/ws")
+        .connect()
+        .await
+        .unwrap();
 
-        client.incoming_messages()
+    log::debug!("response: {res:?}");
+    log::info!("connected; server will echo messages sent");
+    loop {
+        select! {
+            Some(msg) = ws.next() => {
+                match msg {
+                    Ok(ws::Frame::Text(txt)) => {
+                        // log echoed messages from server
+                        log::info!("Server: {:?}", txt)
+                    }
 
-        tokio::spawn(async move {
-            let when = Instant::now() ;
-            let mut interval = time::interval(time::Duration::from_secs(5));
-            loop {
-                interval.tick().await;
-                let message = OwnedMessage::Ping(String::from("ping").into_bytes());
-                client.send_message(&message);
+
+                    _ => {}
+                }
             }
-        });
 
-        // let (res, mut ws) = awc::Client::new()
-        //     .ws("ws://127.0.0.1:8080/ws")
-        //     .connect()
-        //     .await
-        //     .unwrap();
+            Some(cmd) = cmd_rx.next() => {
+                if cmd.is_empty() {
+                    continue;
+                }
 
-        // log::debug!("response: {res:?}");
-        // log::info!("connected; server will echo messages sent");
-        // let timer = timer::Timer::new();
-        // tokio::spawn(async move {
-        //     ws.send(ws::Message::Ping(Bytes::new())).await;
-        // });
-        // loop {
-        //     select! {
-        //         Some(msg) = ws.next() => {
-        //             match msg {
-        //                 Ok(ws::Frame::Text(txt)) => {
-        //                     // log echoed messages from server
-        //                     log::info!("Server: {:?}", txt)
-        //                 }
-        //
-        //                 Ok(ws::Frame::Ping(_)) => {
-        //                     // respond to ping probes
-        //                     ws.send(ws::Message::Pong(Bytes::new())).await.unwrap();
-        //                 }
-        //
-        //                 _ => {}
-        //             }
-        //         }
-        //
-        //         Some(cmd) = cmd_rx.next() => {
-        //             if cmd.is_empty() {
-        //                 continue;
-        //             }
-        //
-        //             ws.send(ws::Message::Text(cmd.into())).await.unwrap();
-        //         }
-        //
-        //         else => break
-        //     }
-        // };
+                if cmd=="ping"{
+                    ws.send(ws::Message::Ping(Bytes::new())).await.unwrap();
+
+                }else {
+                    ws.send(ws::Message::Text(cmd.into())).await.unwrap();
+                }
+
+            }
+
+            else => break
+        }
     }
     input_thread.join().unwrap();
 }
